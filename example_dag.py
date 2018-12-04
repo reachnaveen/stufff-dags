f415b64f3fcdb7b0781d893cf823a60dea822544
Michael Kuehl <michael@ratchet.cc>
2018-12-04 15:12:26 +0100

    added a simple dag

---
+++ example_dag.py
@@ -0,0 +1,16 @@
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': dt.datetime(2018, 12, 1, 10, 00, 00),
+    'concurrency': 1,
+    'retries': 1
+}
+
+dag = DAG('example', default_args=default_args, schedule_interval=timedelta(seconds=120))
+
+opr_hello = BashOperator(task_id='task1', bash_command='echo "Hi!!"')
